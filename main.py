04bd3fe8808596fc96af613efb5649c54e1c07e0
l3onh4rd <46918465+l3onh4rd@users.noreply.github.com>
2024-02-17 15:42:44 +0100

    multiple plots

--- main.py
+++ main.py
@@ -51,27 +51,34 @@ for i in range(1,df_ideal_import.shape[1]):
     plt.clf()
 
 # plot multiple together
-    # Is it possible to plot everything in one plot?
-    # Sort all y datas columnwise
-    # plot sets with nearly equal values
-        # like 0 -10, 10 to 100, 1000 to 10000
 
 # sort dataframe columns by first row
 df_ideal_columns_sorted = df_ideal_import[df_ideal_import.iloc[0].sort_values().index]
-# print(df_ideal_columns_sorted)
 
-plt.plot(df_ideal_import['x'], df_ideal_import['y25'])
-plt.plot(df_ideal_import['x'], df_ideal_import['y24'])
-plt.plot(df_ideal_import['x'], df_ideal_import['y30'])
-plt.plot(df_ideal_import['x'], df_ideal_import['y28'])
-plt.plot(df_ideal_import['x'], df_ideal_import['y21'])
-plt.plot(df_ideal_import['x'], df_ideal_import['y29'])
-plt.plot(df_ideal_import['x'], df_ideal_import['y26'])
+# create masks to access only columns with a specific range of values like bigger or lower than 1000 or -1000
+# by that the functions are comparable in a plot with multiple functions of the same scale
+mask_4 = (df_ideal_columns_sorted.loc[0] > 1000) | (df_ideal_columns_sorted.loc[0] < -1000)
+mask_3 = ((df_ideal_columns_sorted.loc[0] < 1000) & (df_ideal_columns_sorted.loc[0] > 50)) | ((df_ideal_columns_sorted.loc[0] > -1000) & (df_ideal_columns_sorted.loc[0] < -50))
+mask_2 = ((df_ideal_columns_sorted.loc[0] <= 50) & (df_ideal_columns_sorted.loc[0] > 5)) | ((df_ideal_columns_sorted.loc[0] >= -50) & (df_ideal_columns_sorted.loc[0] < -5))
+mask_1 = ((df_ideal_columns_sorted.loc[0] <= 5) & (df_ideal_columns_sorted.loc[0] > 1)) | ((df_ideal_columns_sorted.loc[0] >= -5) & (df_ideal_columns_sorted.loc[0] < 1))
+mask_0 = ((df_ideal_columns_sorted.loc[0] <= 1) & (df_ideal_columns_sorted.loc[0] >= 0)) | ((df_ideal_columns_sorted.loc[0] >= -1) & (df_ideal_columns_sorted.loc[0] <= 0))
 
-plt.plot(df_ideal_import['x'], df_ideal_import['y23'])
-plt.plot(df_ideal_import['x'], df_ideal_import['y22'])
-plt.plot(df_ideal_import['x'], df_ideal_import['y27'])
-plt.show()
+mask_list = [mask_0, mask_1, mask_2, mask_3, mask_4]
+
+# iterate of each mask and then each column to tp combine plots
+for idx, mask in enumerate(mask_list):
+    # convert to mask (pandas Series) to data frame and tranpose it
+    mask_transpose = mask.to_frame().T
+    # use the mask to get the fitting column name (like y25 or y44)
+    column_list = [col for col in mask_transpose.columns if (mask_transpose[col]).all()]
+    # for each fitting column print the row into a plot
+    for func in column_list:
+        plt.plot(df_ideal_import['x'], df_ideal_import[func])
+    # save the combined plot as pdf
+    plt.savefig('./export/ideal_plots_multiple/multiple' + str(idx) + '.pdf')
+    plt.clf()
+
+print(df_ideal_columns_sorted)
 
 '''
 setup
