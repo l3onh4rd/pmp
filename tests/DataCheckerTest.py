a8c81f49728552ebfded072d2d7d423d0d599d6b
l3onh4rd <46918465+l3onh4rd@users.noreply.github.com>
2024-02-17 17:23:33 +0100

    adapted test

--- tests/DataCheckerTest.py
+++ tests/DataCheckerTest.py
@@ -5,16 +5,21 @@ TODO Kommentare
 import unittest
 import pandas as pd
 
-from classes.datachecker import DataChecker
+from datachecker_module.DataChecker import DataChecker
 
 class TestDataChecker(unittest.TestCase):
 
-    test_duplicate_data = pd.read_csv('./tests/data/duplicate.csv')
-    test_missing_data = pd.read_csv('./tests/data/missing.csv')
-    # test_data_checker = DataChecker(test_duplicate_data, 'Test Data')
+    def setUp(self):
+        self.test_duplicate_data = pd.read_csv('./tests/data/duplicate.csv')
+        self.test_missing_data = pd.read_csv('./tests/data/missing.csv')
+        self.test_valid_data = pd.read_csv('./tests/data/valid.csv')
+        self.test_data_checker = DataChecker(self.test_valid_data, 'Test Data')
 
     # def test_get_data_frame(self):
-    #     self.assertEqual(test_data_checker.get_data_frame(), test_duplicate_data)
+    #     self.assertEqual(self.test_data_checker.get_data_frame(), self.test_duplicate_data)
+    
+    def test_duplicate_value(self):
+        self.assertEqual(self.test_data_checker.check_for_duplicate_values(), True)
 
 if __name__ == '__main__':
     unittest.main()
