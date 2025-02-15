c6f0133a7ae12b1cfc1ce1b74cfed9f24f3184c2
l3onh4rd <46918465+l3onh4rd@users.noreply.github.com>
2025-02-15 13:33:08 +0100

    unit test correction

--- tests/DatabaseHandlerTest.py
+++ tests/DatabaseHandlerTest.py
@@ -19,8 +19,8 @@ class DatabaseHandlerTest(unittest.TestCase):
         self.database_handler.close_connection()
 
     def test_get_amount_of_table_columns_and_rows(self):
-        self.assertEqual(self.database_handler.get_amount_of_table_columns_and_rows(), ['Table train_data has 5 columns and 400 rows.', 'Table ideal_data has 51 columns and 400 rows.', 'Table squared_errors_for_best_fitting_functions has 5 columns and 400 rows.'])
-        self.assertListEqual(self.database_handler.get_amount_of_table_columns_and_rows(), ['Table train_data has 5 columns and 400 rows.', 'Table ideal_data has 51 columns and 400 rows.', 'Table squared_errors_for_best_fitting_functions has 5 columns and 400 rows.'])
+        self.assertEqual(self.database_handler.get_amount_of_table_columns_and_rows(), ['Table train_data has 5 columns and 400 rows.', 'Table ideal_data has 51 columns and 400 rows.', 'Table squared_errors_for_best_fitting_functions has 5 columns and 400 rows.', 'Table avg_delta has 4 columns and 4 rows.'])
+        self.assertListEqual(self.database_handler.get_amount_of_table_columns_and_rows(), ['Table train_data has 5 columns and 400 rows.', 'Table ideal_data has 51 columns and 400 rows.', 'Table squared_errors_for_best_fitting_functions has 5 columns and 400 rows.', 'Table avg_delta has 4 columns and 4 rows.'])
 
 if __name__ == '__main__':
     unittest.main()
