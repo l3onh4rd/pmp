ff7e3d07418bb83e709b9e07dbb79c5c8c7c1319
l3onh4rd <46918465+l3onh4rd@users.noreply.github.com>
2025-03-22 15:13:55 +0100

    fix unit tests

--- tests/FunctionDeterminerTest.py
+++ tests/FunctionDeterminerTest.py
@@ -13,16 +13,16 @@ from modules.FunctionDeterminer import FunctionDeterminer
 class FunctionDeterminerTest(unittest.TestCase):
 
     def setUp(self):
-        self.function_determiner = FunctionDeterminer(pd.read_csv('./tests/data/test_train.csv'), pd.read_csv('./tests/data/test_ideal.csv'))
+        self.function_determiner = FunctionDeterminer(pd.read_csv('./tests/data/test_train.csv'), pd.read_csv('./tests/data/test_ideal.csv'), pd.read_csv('./tests/data/b_test.csv'))
     
     def test_print_best_fitting_function(self):
-        self.assertEqual(self.function_determiner.print_best_fitting_function(1, 4, "y2", "y5"), "PROCESSING INFO: (2/4 - 50%) Function y2 fits best to y5")
+        self.assertEqual(self.function_determiner.print_best_fitting_function(1, 4, "y2", "y5", 0.35), "PROCESSING INFO: (2/4 - 50%) Function y2 fits best to y5 (Data set B compare rate: 35.0%)")
 
     def test_calc_least_sq_errors_for_points(self):
         self.assertEqual(self.function_determiner.calc_least_sq_errors_for_points([1,2,3,4,5],[6,3,8,5,9]), [25.0,1.0,25.0,1.0,16.0])
 
     def test_determine_best_fit(self):
-        self.assertEqual(self.function_determiner.determine_best_fit()[0], [('y1', 'y2'),('y2', 'y1')])
+        self.assertEqual(self.function_determiner.determine_best_fit()[0], [('y1', 'y2', 0.0),('y2', 'y1', 1.0)])
 
 if __name__ == '__main__':
     unittest.main()
